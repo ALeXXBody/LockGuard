b47de2374e7a216e12d14daa580aacfd8d416ad6
Bogdan Turlea <93947665+ALeXXBody@users.noreply.github.com>
2026-09-22 12:51:17 +0100

    Add _launch.ps1

---
+++ _launch.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop"
+$errFile = "C:\ProgramData\LockGuard\Logs\crash.log"
+try {
+    & "C:\Users\Bogdan\Documents\Default Project\LockGuard\LockGuard.ps1"
+} catch {
+    "CRASH: $($_.Exception.Message)" | Out-File $errFile
+    "STACK: $($_.ScriptStackTrace)" | Out-File $errFile -Append
+    "TIME: $(Get-Date)" | Out-File $errFile -Append
+}
