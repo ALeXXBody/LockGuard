46e9e4ab1d8ab636ae481e7f7ff36d166aeaeded
Bogdan Turlea <93947665+ALeXXBody@users.noreply.github.com>
2026-09-22 12:51:20 +0100

    Add launch_wrapper.ps1

---
+++ launch_wrapper.ps1
@@ -0,0 +1,8 @@
+﻿try {
+    & "C:\Users\Bogdan\Documents\Default Project\LockGuard\LockGuard.ps1"
+} catch {
+    $errFile = "C:\ProgramData\LockGuard\Logs\error.log"
+    $_.Exception | Out-File $errFile
+    $_.ScriptStackTrace | Out-File $errFile -Append
+    Write-Host "ERROR: $($_.Exception.Message)"
+}
